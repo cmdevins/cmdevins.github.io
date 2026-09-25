8a9d8d93afd50feda450905026d2ee4886d527cf
Xadeka <me@zade.dev>
2026-09-24 20:45:45 -0400

    Split root and page styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./main.css";
 import Home from "./home";
 
 createRoot(document.getElementById("root")).render(
